e13c7f1050a8c899a82a26fdeb8af999c0533667
navyth91 <navyth91@naver.com>
2024-08-21 17:10:09 +0900

    init

---
+++ src/test/java/org/example/basicspringsession/BasicSpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.basicspringsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
